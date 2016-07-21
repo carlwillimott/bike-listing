d15432462af7613a5c5069ae1e5ddc8aa3382a86
Carl Willimott <carlwillimott@googlemail.com>
2016-07-21 21:37:48 +0100

    Added missing files

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {fetchBikes} from '../actions/index';
 import Bike from './Bike';
 
-const dummy = {
-	"id": 1,
-	"name": "Litening C:68",
-	"description": "The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.",
-	"image": {
-		"thumb": "https://jujhar.com/images/bikes/780300_overview.png",
-		"large": "https://jujhar.com/images/bikes/780300_light.jpg"
-	},
-	"class": ["endurance", "race"]
-};
+// const dummy = {
+// 	"id": 1,
+// 	"name": "Litening C:68",
+// 	"description": "The bike for the professionals - thanks to our high-end C:68 Carbon frame and race optimized geometry.",
+// 	"image": {
+// 		"thumb": "https://jujhar.com/images/bikes/780300_overview.png",
+// 		"large": "https://jujhar.com/images/bikes/780300_light.jpg"
+// 	},
+// 	"class": ["endurance", "race"]
+// };
 
 class App extends React.Component {
 
