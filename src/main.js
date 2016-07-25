32631911ba6cb2d1539e1ccebb46ac5e33ebccf6
Carl Willimott <carlwillimott@googlemail.com>
2016-07-25 21:22:54 +0100

    Added some local storage helpers.

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import App from './components/App';
 import {sortBikes} from './reducers'
+import {getPersistedData, setPersistedData} from './store';
 
-let store = createStore(sortBikes);
+let persisted = getPersistedData();
+let store = createStore(sortBikes, persisted);
 
 // Console a list of dispatched actions.
-store.subscribe(() => console.log(store.getState()));
+store.subscribe(() => {
+    let state = store.getState();
+    console.log(state);
+    setPersistedData(state);
+});
 
 ReactDOM.render(
     <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+// Simply store the data in localstorage.
+// @todo - implement a try / catch to deal with read issues.
+export const getPersistedData = () => {
+	const state = localStorage.getItem('bike-listing');
+	if (state === null) {
+		return undefined;
+	}
+	return JSON.parse(state);
+};
+
+export const setPersistedData = (store) => {
+	localStorage.setItem('bike-listing', JSON.stringify(store));
+};
